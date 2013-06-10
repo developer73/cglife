ca6c5503c800aacde97f60d36220bb500bbefcbe
developer73 <developer73@>
2013-06-10 13:46:26 -0600

    * refactoring 'Game' class

--- start_gui.py
+++ start_gui.py
@@ -23,66 +23,55 @@ class Game(wx.Frame):
         self.matrix = Matrix()
 
         self.timer = wx.Timer(self, 1)
-        self.Bind(wx.EVT_TIMER, self.timer_event, self.timer)
-
-        def get_bitmap(id):
-            bitmap = wx.StaticBitmap(self, id=id, bitmap=wx.Bitmap('01.png'))
-            bitmap.Bind(wx.EVT_LEFT_UP, cell_event)
-            return bitmap
-
-        def cell_event(event):
-            pass
-
-        # --- next button
-        def next_event(event):
-            self.next_action()
-        
-        next_button = wx.Button(self, wx.ID_ANY, label = 'Next')
-        next_button.Bind(wx.EVT_BUTTON, next_event)
-
-        # --- play button
-        def play_event(event):
-            print '--- %s Play started.' % str(datetime.now())[:19]
-            self.timer.Start(milliseconds=1000, oneShot=False)
-            play_button.Disable()
-            next_button.Disable()
-            pause_button.Enable()
-
-        play_button = wx.Button(self, wx.ID_ANY, label = 'Play')
-        play_button.Bind(wx.EVT_BUTTON, play_event)
-
-        # --- pause button
-        def pause_event(event):
-            print '--- %s Play paused.' % str(datetime.now())[:19]
-            self.timer.Stop()
-            play_button.Enable()
-            next_button.Enable()
-            pause_button.Disable()
-
-        pause_button = wx.Button(self, wx.ID_ANY, label = 'Pause')
-        pause_button.Disable()
-        pause_button.Bind(wx.EVT_BUTTON, pause_event)
-
-        vbox = wx.BoxSizer(wx.VERTICAL)
+        self.Bind(wx.EVT_TIMER, self.next_event, self.timer)
 
         self.gs = wx.GridSizer(50, 50, 1, 1)
-
-        self.gs.AddMany([get_bitmap(i) for i in range(2500)])
-
-        vbox.Add(self.gs, proportion=1, flag=wx.EXPAND)
-        vbox.Add(next_button, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)
-        vbox.Add(play_button, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)
-        vbox.Add(pause_button, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)
-
-        self.SetSizer(vbox)
-        self.next_action()
-
+        self.gs.AddMany([self.get_bitmap(i) for i in range(2500)])
+        self.create_buttons()
+        self.next_event()
         self.Show(True)
 
-    def timer_event(self, event):
-        self.next_action()
+    def create_buttons(self):
+        self.next_button = wx.Button(self, wx.ID_ANY, label = 'Next')
+        self.next_button.Bind(wx.EVT_BUTTON, self.next_event)
+
+        self.play_button = wx.Button(self, wx.ID_ANY, label = 'Play')
+        self.play_button.Bind(wx.EVT_BUTTON, self.play_event)
+
+        self.pause_button = wx.Button(self, wx.ID_ANY, label = 'Pause')
+        self.pause_button.Disable()
+        self.pause_button.Bind(wx.EVT_BUTTON, self.pause_event)
         
-    def next_action(self):
+        self.add_buttons()
+
+    def add_buttons(self):
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.Add(self.gs, proportion=1, flag=wx.EXPAND)
+        vbox.Add(self.next_button, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)
+        vbox.Add(self.play_button, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)
+        vbox.Add(self.pause_button, flag=wx.EXPAND|wx.TOP|wx.BOTTOM, border=4)
+        self.SetSizer(vbox)
+        
+    def get_bitmap(self, id):
+        bitmap = wx.StaticBitmap(self, id=id, bitmap=wx.Bitmap('01.png'))
+        bitmap.Bind(wx.EVT_LEFT_UP, self.cell_event)
+        return bitmap
+
+    def pause_event(self, event):
+        print '--- %s Play paused.' % str(datetime.now())[:19]
+        self.timer.Stop()
+        self.play_button.Enable()
+        self.next_button.Enable()
+        self.pause_button.Disable()
+
+    def play_event(self, event):
+        print '--- %s Play started.' % str(datetime.now())[:19]
+        self.timer.Start(milliseconds=1000, oneShot=False)
+        self.play_button.Disable()
+        self.next_button.Disable()
+        self.pause_button.Enable()
+
+    def next_event(self, event=None):
         print "--- generation %s" % self.matrix.generation
         for ii in range(0, 50*50-1):
             self.gs.Show(ii)
@@ -90,6 +79,9 @@ class Game(wx.Frame):
             if self.matrix.ml[ii] == self.matrix.live_cell:
                 self.gs.Hide(ii)
         self.matrix.next()
+
+    def cell_event(self, event):
+        pass
 
 app = wx.App()
 Game(None, -1, "Conway's Game of Life")
