2792267e02ef4df50f3998f48ffc18813d7981c2
developer73 <developer73@>
2019-11-22 11:50:38 +0000

    Add tests for cell transformation function

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,7 @@
 import unittest
 
 from cglife.engine import get_neighbours
+from cglife.engine import transform_cell
 
 
 class TestNeighbours(unittest.TestCase):
@@ -44,3 +45,90 @@ class TestNeighbours(unittest.TestCase):
             with self.subTest(index):
                 self.assertEqual(
                     get_neighbours(item["m"], 1, 1, "0"), item["result"])
+
+
+class TestTransformCell(unittest.TestCase):
+
+    def test_transform_cell(self):
+        data = (
+            # dead -> dead
+            {
+                "m": [
+                    [".", ".", ".", "."],
+                    [".", ".", ".", "."],
+                    [".", ".", ".", "."],
+                ],
+                "result": "."
+            },
+            {
+                "m": [
+                    [".", ".", ".", "0"],
+                    [".", ".", ".", "0"],
+                    [".", ".", ".", "0"],
+                ],
+                "result": "."
+            },
+            # dead -> alive
+            {
+                "m": [
+                    [".", "0", ".", "."],
+                    ["0", ".", ".", "."],
+                    [".", "0", ".", "."],
+                ],
+                "result": "0"
+            },
+            # alive -> dead
+            {
+                "m": [
+                    [".", ".", ".", "."],
+                    [".", "0", ".", "."],
+                    [".", ".", ".", "."],
+                ],
+                "result": "."
+            },
+            {
+                "m": [
+                    [".", ".", ".", "0"],
+                    [".", "0", ".", "0"],
+                    [".", ".", ".", "0"],
+                ],
+                "result": "."
+            },
+            {
+                "m": [
+                    [".", ".", ".", "."],
+                    [".", "0", "0", "."],
+                    [".", ".", ".", "."],
+                ],
+                "result": "."
+            },
+            {
+                "m": [
+                    [".", ".", ".", "."],
+                    [".", "0", "0", "."],
+                    [".", ".", "0", "."],
+                ],
+                "result": "0"
+            },
+            {
+                "m": [
+                    [".", ".", "0", "."],
+                    [".", "0", "0", "."],
+                    [".", ".", "0", "."],
+                ],
+                "result": "0"
+            },
+            {
+                "m": [
+                    ["0", ".", "0", "."],
+                    [".", "0", "0", "."],
+                    [".", ".", "0", "."],
+                ],
+                "result": "."
+            },
+        )
+
+        for index, item in enumerate(data):
+            cell = transform_cell(item["m"], 1, 1, "0", ".")
+            with self.subTest(index):
+                self.assertEqual(cell, item["result"])
